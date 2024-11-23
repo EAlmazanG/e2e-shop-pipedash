8bc6c220b4b609f3d16d0d8dd11ab9156bc012bc
Eduardo <almazan94@gmail.com>
2024-11-23 17:16:00 +0100

    finish glue test

--- dags/test_glue_dag.py
+++ dags/test_glue_dag.py
@@ -1,11 +1,24 @@
 import time
+from datetime import timedelta
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 import boto3
 
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import aws_setup
+
 def run_glue_job(**kwargs):
-    glue_client = boto3.client('glue', region_name='eu-west-3')
+    glue_client = boto3.client(
+        'glue',
+        region_name=aws_setup.conf['region_name'],
+        aws_access_key_id=aws_setup.conf['aws_access_key_id'],
+        aws_secret_access_key=aws_setup.conf['aws_secret_access_key']
+    )
 
     response = glue_client.start_job_run(
         JobName='glue_transform'
@@ -13,21 +26,24 @@ def run_glue_job(**kwargs):
     job_run_id = response['JobRunId']
     print(f"Started Glue Job with Run ID: {job_run_id}")
 
+  
     while True:
-        status_response = glue_client.get_job_run(
-            JobName='glue_transform',
-            RunId=job_run_id
-        )
-        state = status_response['JobRun']['JobRunState']
-        print(f"Current Glue Job State: {state}")
-        if state in ['SUCCEEDED', 'FAILED', 'STOPPED']:
-            break
-        time.sleep(30)
+        try:
+            status_response = glue_client.get_job_run(
+                JobName='glue_transform',
+                RunId=job_run_id
+            )
+            state = status_response['JobRun']['JobRunState']
+            print(f"Current Glue Job State: {state}")
+            
+            if state in ['SUCCEEDED', 'FAILED', 'STOPPED']:
+                break
 
-    if state != 'SUCCEEDED':
-        raise Exception(f"Glue Job failed with state: {state}")
+            time.sleep(30)
 
-    print("Glue Job completed successfully.")
+        except Exception as e:
+            print(f"Error while monitoring Glue Job: {e}")
+            raise
 
 with DAG(
     dag_id="test_aws_glue",
@@ -35,10 +51,11 @@ with DAG(
     start_date=days_ago(1),
     catchup=False,
     tags=["aws", "glue"],
+    dagrun_timeout=timedelta(hours=1)
 ) as dag:
 
     glue_task = PythonOperator(
         task_id="test_glue_job",
         python_callable=run_glue_job,
-        provide_context=True,  # Habilita el paso de contexto si necesitas kwargs
+        provide_context=True,
     )
