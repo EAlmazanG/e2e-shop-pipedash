fdcd535a7ac8c08d488a105e837fc9d5b2f81844
Eduardo <almazan94@gmail.com>
2024-11-26 00:48:06 +0100

    clean schemas

--- dags/test_snowflake.py
+++ dags/test_snowflake.py
@@ -13,7 +13,7 @@ default_args = {
 
 IMPORT_TABLES = {
     "dim_items": """
-        COPY INTO public.dim_items
+        COPY INTO dim_items
         FROM (
             SELECT
                 $1:item_uuid::STRING AS item_uuid,
@@ -29,12 +29,22 @@ IMPORT_TABLES = {
             FROM @aws_s3_stage/dim_items/
         )
         FILE_FORMAT = (TYPE = PARQUET)
+    """,
+    "dim_customers": """
+        COPY INTO dim_customers
+        FROM (
+            SELECT
+                $1:customer_id::INT AS customer_id,
+                $1:customer_name::STRING AS customer_name
+            FROM @aws_s3_stage/dim_customers/
+        )
+        FILE_FORMAT = (TYPE = PARQUET)
     """
 }
 
 GENERATE_INT_TABLES = {
     "int_items": """
-        create or replace table public.int_items as
+        create or replace table int_items as
         select 
             ft.item_uuid,
             di.item_description,
@@ -51,10 +61,10 @@ GENERATE_INT_TABLES = {
             sum(ft.quantity_amount) as total_quantity,
             count(distinct ft.transaction_id) as total_transactions
         from 
-            public.fact_transactions as ft
-            inner join public.dim_items as di on 
+            fact_transactions as ft
+            inner join dim_items as di on 
                 ft.item_uuid = di.item_uuid
-            inner join public.dim_item_family as df on 
+            inner join dim_item_family as df on 
                 di.item_family_id = df.item_family_id
         group by 
             ft.item_uuid, 
@@ -67,13 +77,13 @@ GENERATE_INT_TABLES = {
             di.is_special_item, 
             di.is_modification_item, 
             di.is_error_item;
-    """
+    """,
 }
 
 GENERATE_ETLS = {
     "etl_items": """
-        create or replace table bi.etl_items as
-        select * from public.int_items;
+        create or replace table etl_items as
+        select * from int_items;
     """
 }
 
@@ -95,3 +105,21 @@ with DAG(
             sql=query,
         )
         import_tasks.append(task)
+
+    generate_int_tables_tasks = []
+    for table, query in GENERATE_INT_TABLES.items():
+        task = SnowflakeOperator(
+            task_id=f"generate_{table}",
+            snowflake_conn_id="snowflake_default",
+            sql=query,
+        )
+        generate_int_tables_tasks.append(task)
+
+    generate_etl_tasks = []
+    for table, query in GENERATE_ETLS.items():
+        task = SnowflakeOperator(
+            task_id=f"generate_{table}",
+            snowflake_conn_id="snowflake_default",
+            sql=query,
+        )
+        generate_etl_tasks.append(task)
